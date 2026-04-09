a25c3110ff00d85378f4e201805c2beca15af4e1
Garcia59 <20261ctb0100044@estudantes.ifpr.edu.br>
2026-04-09 20:06:22 +0000

    Aula 11 - 1009: salario com bonus

---
+++ beecrowd/1003-soma-simples.py
@@ -0,0 +1,21 @@
+'''
+Problema: beecrowd | 1003
+Data: 2026.04.07
+Estudante: Matheus Felipe Garcia
+'''
+#Objetivo: Somar dois valores
+
+# ---ANÁLISE (LIAC) ---
+# Entrada: dois números inteiros cada um em uma linha 
+# Processamento: somar A + B e armazenar em SOMA =
+# Saída: exibir no formato exato "SOMA = valor"
+
+# Leitura das variáveis (observe o enunciado - quantas entradas há?)
+A = 1
+B = 1
+
+# Cálculo
+ + SOMA = 2
+
+# Saída - observe o formato exato no enunciado 
+print()
