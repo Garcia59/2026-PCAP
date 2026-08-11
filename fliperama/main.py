03fd242098596260d53f00825f5decb8c032e5fd
Garcia59 <20261ctb0100044@estudantes.ifpr.edu.br>
2026-08-11 22:22:13 +0000

    fliperama

--- fliperama/main.py
+++ fliperama/main.py
@@ -7,24 +7,45 @@
 # Conceitos:  []
 # =============================================
 
-# importar funções de arquivo (módulos)
 from telas import titulo, linha
 from adivinhe import jogar_adivinhe
+from ppt import jogar_ppt
 from modulos import ler_opcao
 
 NOME_DO_DONO = 'MATHEUS.FELIPE'
-OPCOES = ['0', '1']
+OPCOES = ['0', '1', '2']
+NOMES_DOS_JOGOS = ['Adivinhe o Numero', 'Pedra-Papel-Tesoura', 'Par ou Impar']
+vezes_jogado = [0, 0, 0]
+
+
+def mostrar_placar():
+    titulo('PLACAR')
+
+    for i in range(3):
+        print(NOMES_DOS_JOGOS[i] + ': ' + str(vezes_jogado[i]) + 'x')
+
 
 while True:
     titulo('FLIPERAMA DO ' + NOME_DO_DONO)
+
     print('1 - Jogo Adivinhe o Número')
+    print('2 - Pedra-Papel-Tesoura')
     print('0 - Sair do Fliperama')
+
     linha()
+
     opcao = ler_opcao('Escolha uma opção', OPCOES)
 
     if opcao == '0':
-        print('Até a Próxima!')
+        mostrar_placar()
+        titulo('Ate a proxima!')
         break
+
     elif opcao == '1':
         jogar_adivinhe()
- 
+
+    elif opcao == '2':
+        jogar_ppt()
+
+    indice = int(opcao) - 1
+    vezes_jogado[indice] = vezes_jogado[indice] + 1
